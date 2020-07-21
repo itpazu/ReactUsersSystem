232424bb137fee6e1ae4d7224a9e5dbc8d31c169
itpazu <&;/39euJaA##xZ/>
2020-07-21 15:25:54 +0300

    merges and conflicts

--- my-app/src/Routes.js
+++ my-app/src/Routes.js
@@ -1,45 +1,50 @@
-import React, { useState } from 'react'
-import { Switch, Redirect, Route } from 'react-router-dom'
-import Main from './Layouts/main/main'
-import LoginForm from './components/LoginForm.jsx'
-import cookie from 'js-cookie'
-import { LogIn, authenticateUser } from './lib/api'
-import NavBar from './Layouts/main/components/Navbar'
+import React, { useState } from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import Main from './Layouts/main/main';
+import LogIn from './views/LogIn';
+import cookie from 'js-cookie';
+import { authenticateUser } from './lib/api';
+import Minimal from './Layouts/minimal/Minimal';
 
 const Routes = () => {
-    const [userInput, setUserInput] = useState({ email: '', password: '' })
-    const [token, setToken] = useState(null)
+  const [userInput, setUserInput] = useState({ email: '', password: '' });
+  const [token, setToken] = useState(null);
 
-    const handleSubmittedForm = async (e) => {
-        e.preventDefault()
-        const response = await LogIn(userInput)
-        const userId = response.data.user_id
-        const csrfToken = response.headers.authorization
-        cookie.set('user_id', userId)
-        cookie.set('csrf_token', csrfToken)
-        handleFetch()
-    }
+  const handleSubmittedForm = async (e) => {
+    e.preventDefault();
+    const response = await LogIn(userInput);
+    const userId = response.data.user_id;
+    const csrfToken = response.headers.authorization;
+    cookie.set('user_id', userId);
+    cookie.set('csrf_token', csrfToken);
+    handleFetch();
+  };
 
-    const handleFetch = async () => {
-        const userId = cookie.get('user_id')
-        const csrf = cookie.get('csrf_token')
-        const getToken = await authenticateUser({ user_id: userId }, csrf)
-        setToken(getToken)
-    }
+  const handleFetch = async () => {
+    const userId = cookie.get('user_id');
+    const csrf = cookie.get('csrf_token');
+    const getToken = await authenticateUser({ user_id: userId }, csrf);
+    setToken(getToken);
+  };
 
-    const handleLogout = () => {
-        cookie.remove('csrf_token')
-        setToken(null)
-    }
+  const handleLogout = () => {
+    cookie.remove('csrf_token');
+    setToken(null);
+  };
 
-    return (
-        <Switch>
-            <Route exact path="/">
-                <LoginForm handleSubmittedForm={handleSubmittedForm} setUserInput={setUserInput} userInput={userInput} />
-                <Main token={token} handleLogout={handleLogout} />
-            </Route>
-        </Switch>
-    )
-}
+  return (
+    <Switch>
+      <Route exact path='/'>
+        {/* <LoginForm handleSubmittedForm={handleSubmittedForm} setUserInput={setUserInput} userInput={userInput} /> */}
+        <Main token={token} handleLogout={handleLogout} />
+      </Route>
+      <Route exact path='/login'>
+        <Minimal>
+          <LogIn />
+        </Minimal>
+      </Route>
+    </Switch>
+  );
+};
 
-export default Routes
+export default Routes;
