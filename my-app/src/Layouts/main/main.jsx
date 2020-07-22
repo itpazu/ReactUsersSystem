3e4312b412c792cfe93c6767e888f0613b4e08b5
itpazu <&;/39euJaA##xZ/>
2020-07-22 10:40:25 +0300

    routing

--- my-app/src/Layouts/main/main.jsx
+++ my-app/src/Layouts/main/main.jsx
@@ -1,65 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { makeStyles, useTheme } from '@material-ui/styles';
 import { useMediaQuery } from '@material-ui/core';
 import TopBar from './components/TopBar/TopBar';
 import Sidebar from './components/sidebar/sidebar';
+import Context from '../../context/Context';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     paddingTop: 56,
     height: '100%',
     [theme.breakpoints.up('sm')]: {
-      paddingTop: 64
-    }
+      paddingTop: 64,
+    },
   },
   shiftContent: {
-    paddingLeft: 240
+    paddingLeft: 240,
   },
   content: {
-    height: '100%'
-  }
-}))
+    height: '100%',
+  },
+}));
 
 const Main = (props) => {
-  const { children, token, handleLogout } = props
-
-  const classes = useStyles()
-  const theme = useTheme()
+  const { children, token, handleLogout } = props;
+  const context = useContext(Context);
+  const classes = useStyles();
+  const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up('lg'), {
-    defaultMatches: true
-  })
+    defaultMatches: true,
+  });
 
-  const [openSidebar, setOpenSidebar] = useState(false)
+  const [openSidebar, setOpenSidebar] = useState(false);
 
   const handleSidebarOpen = () => {
-    setOpenSidebar(true)
-  }
+    setOpenSidebar(true);
+  };
 
   const handleSidebarClose = () => {
-    setOpenSidebar(false)
-  }
+    setOpenSidebar(false);
+  };
 
-  const shouldOpenSidebar = isDesktop ? true : openSidebar
-
+  const shouldOpenSidebar = isDesktop ? true : openSidebar;
+  console.log(context.isAuthenticated);
   return (
-    <div
-      className={{
-        [classes.root]: true,
-        [classes.shiftContent]: isDesktop
-      }}
-    >
-      <TopBar onSidebarOpen={handleSidebarOpen} token={token} handleLogout={handleLogout} />
-      {token &&
-        <>
+    <>
+      {context.isAuthenticated && (
+        <div
+          className={{
+            [classes.root]: true,
+            [classes.shiftContent]: isDesktop,
+          }}
+        >
+          <TopBar
+            onSidebarOpen={handleSidebarOpen}
+            handleLogout={handleLogout}
+          />
+
           <Sidebar
             onClose={handleSidebarClose}
             open={shouldOpenSidebar}
             variant={isDesktop ? 'persistent' : 'temporary'}
           />
           <main className={classes.content}>{children}</main>
-        </>}
-    </div>
-  )
-}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Main
+export default Main;
--- my-app/src/Routes.js
+++ my-app/src/Routes.js
@@ -8,26 +8,11 @@ import Minimal from './Layouts/minimal/Minimal';
 import Context from './context/Context';
 
 const Routes = () => {
-  //   const [userInput, setUserInput] = useState({ email: '', password: '' });
-  const [token, setToken] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [cookiesState, setCookiesState] = useState(false);
-
-  //   useEffect(async () => {
-  //     try {
-  //       const userId = cookie.get('user_id');
-  //       const csrf = cookie.get('csrf_token');
-  //       const getToken = await authenticateUser({ user_id: userId }, csrf);
-  //       setToken(getToken);
-  //       setIsAuthenticated(true);
-  //     } catch (error) {
-  //       setIsAuthenticated(false);
-  //       console.log(error)
-  //     }
-  //   }, [cookiesState]);
+  const [userInput, setUserInput] = useState({ email: '', password: '' });
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
 
   const handleSubmittedForm = async (mail, pass) => {
-    // setUserInput({email: mail, password: pass})
+    setUserInput({ email: mail, password: pass });
     const userInput = { email: mail, password: pass };
     const response = await LogIn(userInput);
     const userId = response.data.user_id;
@@ -35,23 +20,19 @@ const Routes = () => {
     cookie.set('user_id', userId);
     cookie.set('csrf_token', csrfToken);
     handleFetch();
-    setCookiesState(true);
   };
-  console.log(token);
   const handleFetch = async () => {
     const userId = cookie.get('user_id');
     const csrf = cookie.get('csrf_token');
     const getToken = await authenticateUser({ user_id: userId }, csrf);
-    setToken(true);
+    setIsAuthenticated(true);
   };
 
   return (
     <Switch>
-      <Context.Provider value={{ handleSubmittedForm }}>
+      <Context.Provider value={{ handleSubmittedForm, isAuthenticated }}>
         <Route exact path='/'>
-          {/* <LoginForm handleSubmittedForm={handleSubmittedForm} setUserInput={setUserInput} userInput={userInput} /> */}
-          <Main token={token} />
-          {/* <div>hello world</div> */}
+          <Main />
         </Route>
         <Route exact path='/login'>
           <Minimal>
