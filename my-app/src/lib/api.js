0fe302856f08e3c4d4e2f78d674b8d273d233034
David Bador <david.bador@gmail.com>
2020-08-05 17:16:32 +0300

    reformatted file for es6 rules

--- my-app/src/lib/api.js
+++ my-app/src/lib/api.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const baseURL =
-  'https://keepershomestaging-env.eba-b9pnmwmp.eu-central-1.elasticbeanstalk.com';
+  'https://keepershomestaging-env.eba-b9pnmwmp.eu-central-1.elasticbeanstalk.com'
 
 // const baseURL = 'http://127.0.0.1:5000'
 
 export const LogIn = (data) => {
-  return axios.post(`${baseURL}/login`, data, { withCredentials: true });
-};
+  return axios.post(`${baseURL}/login`, data, { withCredentials: true })
+}
 
 // // remote
 // export const authenticateUser = (data, csrf) => {
@@ -22,15 +22,15 @@ export const LogIn = (data) => {
 // };
 
 export const refreshToken = (userId) => {
-  const data = { _id: userId };
+  const data = { _id: userId }
   const headers = {
     headers: {
-      credentials: 'cross-site',
+      credentials: 'cross-site'
     },
-    withCredentials: true,
-  };
-  return axios.post(`${baseURL}/refresh_token`, data, headers);
-};
+    withCredentials: true
+  }
+  return axios.post(`${baseURL}/refresh_token`, data, headers)
+}
 
 // for local server only:
 // export const authenticateUser = (data, csrf, JwtToken) => {
@@ -47,65 +47,66 @@ export const refreshToken = (userId) => {
 // }
 
 export const Logout = () => {
-  return axios.get(`${baseURL}/logout`);
-};
+  return axios.get(`${baseURL}/logout`)
+}
 
 export const register = (newUser, authenticationInfo) => {
-  const csrf = authenticationInfo.csrf_token;
-  const body = newUser;
-  body._id = authenticationInfo._id;
+  const csrf = authenticationInfo.csrf_token
+  const body = newUser
+  body._id = authenticationInfo._id
   // const JwtToken = authenticationInfo.Jwt_token; //local serverOnly
   const headers = {
     headers: {
       credentials: 'cross-site',
-      Authorization: csrf,
+      Authorization: csrf
       // token: JwtToken, local server only
     },
-    withCredentials: true,
-  };
-  return axios.post(`${baseURL}/add_user`, body, headers);
-};
+    withCredentials: true
+  }
+  return axios.post(`${baseURL}/add_user`, body, headers)
+}
 
 export const deleteUser = (userId, authenticationInfo) => {
-  const body = { user_id: userId, _id: authenticationInfo._id };
+  const body = { user_id: userId, _id: authenticationInfo._id }
 
   const data = {
     headers: {
       credentials: 'cross-site',
-      Authorization: authenticationInfo.csrf_token,
+      Authorization: authenticationInfo.csrf_token
     },
     withCredentials: true,
-    data: body,
-  };
-  return axios.delete(`${baseURL}/delete_user`, data);
-};
+    data: body
+  }
+  return axios.delete(`${baseURL}/delete_user`, data)
+}
 
 export const allUsers = (userId, csrf) => {
-  const body = { _id: userId };
+  const body = { _id: userId }
   const headers = {
     headers: {
       credentials: 'cross-site',
-      Authorization: csrf,
+      Authorization: csrf
     },
-    withCredentials: true,
-  };
-  return axios.post(`${baseURL}/all_users`, body, headers);
-};
+    withCredentials: true
+  }
+  return axios.post(`${baseURL}/all_users`, body, headers)
+}
 
 export const checkTokenForPasswordReset = (userId, token) => {
-  console.log(userId, token);
-  const body = { _id: userId };
+  console.log(userId, token)
+  const body = { _id: userId }
   const headers = {
     headers: {
-      token: token,
-    },
-  };
-  return axios.post(`${baseURL}/check_token`, body, headers);
-};
+      token: token
+    }
+  }
+  return axios.post(`${baseURL}/check_token`, body, headers)
+}
 
 export const changePassword = (data) => {
-  return axios.post(`${baseURL}/change_password`, data);
-};
+  return axios.post(`${baseURL}/change_password`, data)
+}
+
 export const solicitNewPassword = (data) => {
-  return axios.post(`${baseURL}/newpass_solicit`, data);
-};
+  return axios.post(`${baseURL}/newpass_solicit`, data)
+}
