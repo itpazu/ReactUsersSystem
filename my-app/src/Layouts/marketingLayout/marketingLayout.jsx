6849dfd91aa588b45133c753eeecfff1c5d4cef9
David Bador <david.bador@gmail.com>
2020-08-09 10:59:45 +0300

    Added the beginning of more role layouts for KeepersHome

---
+++ my-app/src/Layouts/marketingLayout/marketingLayout.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { makeStyles, useTheme } from '@material-ui/styles'
+import { useMediaQuery } from '@material-ui/core'
+import TopBar from './components/TopBar/TopBar'
+import Sidebar from './components/sidebar/sidebar'
+import clsx from 'clsx'
+import HomePage from '../../views/HomePage/HomePage'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { PrivateRoute } from '../../privateRoutes/PrivateRoute'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: 56,
+    height: '-webkit-fill-available',
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: 64
+    }
+  },
+  shiftContent: {
+    paddingLeft: 240
+  },
+  content: {
+    height: '100%'
+  }
+}))
+
+const Main = () => {
+  const classes = useStyles()
+  const theme = useTheme()
+  const isDesktop = useMediaQuery(theme.breakpoints.up('lg'), {
+    defaultMatches: true
+  })
+
+  const [openSidebar, setOpenSidebar] = useState(false)
+
+  const handleSidebarOpen = () => {
+    setOpenSidebar(true)
+  }
+
+  const handleSidebarClose = () => {
+    setOpenSidebar(false)
+  }
+
+  const shouldOpenSidebar = isDesktop ? true : openSidebar
+
+  return (
+    <Router>
+      <div
+        className={clsx({
+          [classes.root]: true,
+          [classes.shiftContent]: isDesktop
+        })}
+      >
+        <TopBar onSidebarOpen={handleSidebarOpen} />
+
+        <Sidebar
+          onClose={handleSidebarClose}
+          open={shouldOpenSidebar}
+          variant={isDesktop ? 'persistent' : 'temporary'}
+        />
+        <main className={classes.content}>
+          <PrivateRoute exact path='/' component={HomePage} />
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default Main
---
+++ my-app/src/Layouts/productManagerLayout/components/sidebar/sidebar.jsx
@@ -0,0 +1,106 @@
+import React, { useContext } from 'react'
+import { makeStyles } from '@material-ui/styles'
+import { Divider, Drawer } from '@material-ui/core'
+import DashboardIcon from '@material-ui/icons/Dashboard'
+import AccountBoxIcon from '@material-ui/icons/AccountBox'
+import SettingsIcon from '@material-ui/icons/Settings'
+import Profile from './profile/profile'
+import SidebarNav from './sideBarNav/sideBarNav'
+import Context from '../../../../context/Context'
+import WeekendIcon from '@material-ui/icons/Weekend'
+import ListAltIcon from '@material-ui/icons/ListAlt'
+import EqualizerIcon from '@material-ui/icons/Equalizer'
+import CodeIcon from '@material-ui/icons/Code'
+import InputIcon from '@material-ui/icons/Input'
+
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    width: 240,
+    [theme.breakpoints.up('lg')]: {
+      marginTop: 64,
+      height: 'calc(100% - 64px)'
+    }
+  },
+  root: {
+    backgroundColor: theme.palette.white,
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    padding: theme.spacing(2)
+  },
+  divider: {
+    margin: theme.spacing(2, 0)
+  },
+  nav: {
+    marginBottom: theme.spacing(2)
+  }
+}))
+
+const Sidebar = (props) => {
+  const { open, variant, onClose, ...rest } = props
+
+  const classes = useStyles()
+
+  const context = useContext(Context)
+
+  const pages = [
+    {
+      title: 'Dashboard',
+      href: '/dashboard',
+      icon: <DashboardIcon />
+    },
+    {
+      title: 'BI System',
+      href: '/system',
+      icon: <CodeIcon />
+    },
+    {
+      title: 'DB User Actions',
+      href: '/actions',
+      icon: <ListAltIcon />
+    },
+    {
+      title: 'The Lounge',
+      href: '/lounge',
+      icon: <WeekendIcon />
+    },
+    {
+      title: 'Server Statistics',
+      href: '/stats',
+      icon: <EqualizerIcon />
+    },
+    {
+      title: 'Account',
+      href: '/account',
+      icon: <AccountBoxIcon />
+    },
+    {
+      title: 'Settings',
+      href: '/settings',
+      icon: <SettingsIcon />
+    },
+    {
+      title: 'Log Out',
+      href: '/login',
+      icon: <InputIcon />
+    }
+  ]
+
+  return (
+    <Drawer
+      anchor='left'
+      classes={{ paper: classes.drawer }}
+      onClose={onClose}
+      open={open}
+      variant={variant}
+    >
+      <div {...rest} className={classes.root}>
+        <Profile profile={context.userInput} />
+        <Divider className={classes.divider} />
+        <SidebarNav className={classes.nav} pages={pages} />
+      </div>
+    </Drawer>
+  )
+}
+
+export default Sidebar
