94d0fdc6c5352005b6942b635d1178b7a4919392
itpazu <&;/39euJaA##xZ/>
2020-08-23 16:33:50 +0300

    more DRY

--- my-app/src/views/AccountPage/Account.js
+++ my-app/src/views/AccountPage/Account.js
@@ -2,8 +2,9 @@ import React, { useContext, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
 import Context from '../../context/Context';
-
+import Alert from '@material-ui/lab/Alert';
 import { AccountProfile, AccountDetails } from './components';
+import { getUserInfoRefresh } from '../../lib/api';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,7 +15,8 @@ const useStyles = makeStyles((theme) => ({
 const Account = () => {
   const classes = useStyles();
   const context = useContext(Context);
-  const { updateProfileInfo, userInput } = context;
+  const { updateProfileInfo, errorUpdateProfile } = context;
+
   useEffect(() => {
     updateProfileInfo();
   }, []);
@@ -28,6 +30,14 @@ const Account = () => {
         <Grid item lg={8} md={6} xl={8} xs={12}>
           <AccountDetails />
         </Grid>
+
+        {errorUpdateProfile && (
+          <Grid xs={12}>
+            <Alert className={classes.alertMessage} severity='error'>
+              {errorUpdateProfile.message}
+            </Alert>
+          </Grid>
+        )}
       </Grid>
     </div>
   );
