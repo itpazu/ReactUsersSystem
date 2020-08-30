3be88c5c364a1cf7c154bbb3768c74abd2acad03
itpazu <&;/39euJaA##xZ/>
2020-08-30 15:31:53 +0300

    push before deployment

--- my-app/src/views/UserList/components/UsersTable/dialogUnblock.jsx
+++ my-app/src/views/UserList/components/UsersTable/dialogUnblock.jsx
@@ -1,27 +1,27 @@
-import React, { useState, useContext } from 'react'
-import { Button, Dialog } from '@material-ui/core'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import { unblockSystemUser } from '../../../../lib/api'
-import Context from '../../../../context/Context'
-import Alert from '@material-ui/lab/Alert'
+import React, { useState, useContext } from 'react';
+import { Button, Dialog } from '@material-ui/core';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { unblockSystemUser } from '../../../../lib/api';
+import Context from '../../../../context/Context';
+import Alert from '@material-ui/lab/Alert';
 
 const DialogUnblock = (props) => {
-  const { closeUnblock, OpenDialog, userDetails, getAllUsers } = props
-  const { blockedUserId, userName, emailAddress } = userDetails
+  const { closeUnblock, OpenDialog, userDetails, getAllUsers } = props;
+  const { blockedUserId, userName, emailAddress } = userDetails;
 
   const handleClose = () => {
-    closeUnblock()
-  }
+    closeUnblock();
+  };
 
-  const context = useContext(Context)
-  const { makeApiRequest } = context
+  const context = useContext(Context);
+  const { makeApiRequest } = context;
   const [activateAlert, setActivateAlert] = useState({
     activate: false,
-    message: null
-  })
+    message: null,
+  });
 
   const handleUnblock = async () => {
     await makeApiRequest(
@@ -30,8 +30,9 @@ const DialogUnblock = (props) => {
       getAllUsers,
       handleUnblock,
       setActivateAlert
-    )
-  }
+    );
+    handleClose();
+  };
 
   return (
     <div>
@@ -65,7 +66,7 @@ const DialogUnblock = (props) => {
         )}
       </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default DialogUnblock
+export default DialogUnblock;
