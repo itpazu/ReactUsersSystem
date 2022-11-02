11e07cacc74c4391e4c39b4eba502c7f441106d0
itpazu <itpazu@gmail.com>
2022-11-02 13:47:11 +0200

    update basic url for new server in vercel

--- my-app/src/lib/api.js
+++ my-app/src/lib/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://127.0.0.1:5000'
-    : 'https://hogwarts-itpazu.herokuapp.com';
+    : 'https://flask-login-system-theta.vercel.app/';
 
 export const LogIn = (data) => {
   return axios.post(`${baseURL}/login`, data, {
